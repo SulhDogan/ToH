9f8114e6c3c5b776e214a75d988af884ee1c0be2
Yahya GEDİK <yahyagedikyg@gmail.com>
2018-10-15 13:37:07 +0000

    7ye kadar

---
+++ src/app/message.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
